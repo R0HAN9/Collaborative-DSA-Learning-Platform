4e7bdde5c60929479b5e5c4bd14b9f893115dcdc
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2025-01-03 11:27:22 +0530

    Update TechGuideController.java

--- src/main/java/com/Collaborative/DSALearningPlatform/Controllers/TechGuideController.java
+++ src/main/java/com/Collaborative/DSALearningPlatform/Controllers/TechGuideController.java
@@ -7,16 +7,29 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Controller for providing technology guide links.
+ * This controller serves endpoints to fetch curated links for learning and interview preparation.
+ */
 @RestController
 public class TechGuideController {
 
+    /**
+     * Endpoint to fetch a list of curated technology guide links.
+     *
+     * @return A list of {@link TechGuideLink} objects containing the title and URL of each guide.
+     */
     @GetMapping("/techGuide")
     public List<TechGuideLink> getTechGuideLinks() {
-        // Return a list of links
+        // Initialize a list to store tech guide links
         List<TechGuideLink> links = new ArrayList<>();
+
+        // Add curated links to the list
         links.add(new TechGuideLink("Google Tech Dev Guide", "https://techdevguide.withgoogle.com/"));
         links.add(new TechGuideLink("DSA Problems in Java", "https://techdevguide.withgoogle.com/paths/data-structures-and-algorithms/?programming_languages=java"));
         links.add(new TechGuideLink("Interview Preparation", "https://techdevguide.withgoogle.com/paths/interview/?programming_languages=java"));
+
+        // Return the list of links
         return links;
     }
 }
